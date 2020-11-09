2f49985fa35dcd0449611b53b7323ae19423cf60
kanakan12 <69573636+kanakan12@users.noreply.github.com>
2020-11-09 20:53:01 +0800

    Add files via upload

---
+++ 1449.cpp
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N, L;
+int arr[1001];
+int ans;
+
+int main()
+{
+	scanf("%d %d", &N, &L);
+
+	for (int i = 0; i < N; i++)
+		scanf("%d", &arr[i]);
+
+	sort(arr, arr + N);
+
+	int idx = 0;
+	int cur;
+	while (idx != N)
+	{
+		cur = arr[idx] + L;
+
+		while (cur > arr[idx] && idx < N)
+			idx++;
+
+		ans++;
+	}
+
+	printf("%d", ans);
+}
