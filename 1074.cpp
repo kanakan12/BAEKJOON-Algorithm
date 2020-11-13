42e4eedbf87dc317956f5f0a3bf69440a0615659
kanakan12 <69573636+kanakan12@users.noreply.github.com>
2020-11-13 19:42:04 +0800

    Add files via upload

---
+++ 1074.cpp
@@ -0,0 +1,42 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int N, r, c;
+int ans = 0;
+
+void find(int x, int y, int n)
+{
+	if (x == r && y == c)
+	{
+		printf("%d", ans);
+		return;
+	}
+
+	if (x <= r && r < x + n && y <= c && c < y + n)
+	{
+		for (int i = x; i < x + n; i += n / 2)
+		{
+			for (int j = y; j < y + n; j += n / 2)
+			{
+				find(i, j, n / 2);
+			}
+		}
+	}
+	else
+	{
+		ans += n * n;
+	}
+}
+
+int main()
+{
+	scanf("%d %d %d", &N, &r, &c);
+	N = pow(2, N);
+	
+	find(0, 0, N);
+}
