343a19733a04f93c8bbf9eaec79a8852b276fd32
kanakan12 <69573636+kanakan12@users.noreply.github.com>
2020-12-01 21:24:39 +0900

    Add files via upload

---
+++ 11726.cpp
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <algorithm>
+
+int main()
+{
+	int N;
+	scanf("%d", &N);
+
+	if (N == 1 || N == 2) printf("%d", N);
+	else
+	{
+		int a = 1, b = 2, c;
+		for (int i = 0; i < N - 2; i++)
+		{
+			c = (a + b) % 10007;
+			a = b;
+			b = c;
+		}
+		printf("%d", c);
+	}
+}
